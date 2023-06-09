9926062bebb137087ce51c028203194a54338e54
orchidromdev <126561359+theprojectsorchid@users.noreply.github.com>
2023-06-09 02:25:31 +0100

    OrchidOs vendor prebuilts : 09/06/2023
    OrchidOs vendor prebuilts : 09/06/2023
    
    import latest version of applications
    these are core system applications
    some are hidden in lawnchair

---
+++ fossprebuilts.mk
@@ -0,0 +1,26 @@
+# Apps
+PRODUCT_PACKAGES += \
+	AdAway \
+	aperture \
+	appman \
+	athena \
+	aves \
+	BromiteSystemWebview \
+	cal \
+	cleanerplus \
+	connectyou \
+	datamonitor \
+	dualwall \
+	datamonitor \
+	gpt \
+	imagetool \
+	libretube \
+	prow \
+	qstiles \
+	qksms \
+	recordyou \
+	rethink \
+	taptap \
+	Telegram \
+	translate \
+	wifiinfo
